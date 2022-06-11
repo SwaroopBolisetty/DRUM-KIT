d873711b0202a43a61a5febb59420b1428189b0a
BOLISETTY JYOTHI SWAROOP <87690969+SwaroopBolisetty@users.noreply.github.com>
2022-06-11 13:15:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function func2(){
+  var audio=new Audio("./sounds/tom-4.mp3");
+  audio.play()
+}
+function func3(){
+  var audio=new Audio("./sounds/kick-bass.mp3");
+  audio.play()
+}
+function func4(){
+  var audio=new Audio("./sounds/snare.mp3");
+  audio.play()
+}
+function func5(){
+  var audio=new Audio("./sounds/tom-1.mp3");
+  audio.play()
+}
+function func6(){
+  var audio=new Audio("./sounds/tom-2.mp3");
+  audio.play()
+}
+function func7(){
+  var audio=new Audio("./sounds/crash.mp3");
+  audio.play()
+}
+function func1(){
+  var audio=new Audio("./sounds/tom-3.mp3");
+  audio.play()
+}
